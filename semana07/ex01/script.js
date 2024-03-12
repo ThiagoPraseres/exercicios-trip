6ab5287682d1841bf551afec3b2a5d4ee2cad127
Thiago Praseres <thiago_praseres@estudante.sesisenai.org.br>
2024-03-11 21:21:27 -0300

    Exercicios da Semana

---
+++ semana07/ex01/script.js
@@ -0,0 +1,13 @@
+class Produto {
+    nome;
+    preco;
+    quantidade;
+}
+
+let produtoNovo = new Produto()
+produtoNovo.nome = "Carro"
+produtoNovo.preco = 10000
+produtoNovo.quantidade = 1
+
+
+console.log(produtoNovo)
